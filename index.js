58cd429885c4ac8242271b290114a21db0faa844
fpatel3 <forampatel3010@gmail.com>
2021-03-12 12:25:47 -0500

    class 1 for node finished

--- index.js
+++ index.js
@@ -11,14 +11,41 @@
  
 const express = require('express')
 
+const morgan = require('morgan')
+const bodyParser = require('body-parser');
 const app = express()
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+//app.use(morgan('combined'));
+
 
 app.get('/', (request, response) => {
 
-    // console.log("Request received")
+     //console.log("Request received")
     // response.send('Welcome');
     //response.json({msg: 'welcome'});
-    //response.end()
+    
+
+    //Query Parameters
+    // console.log('Query Parameters');
+    // console.log(request.query)
+
+    //Body Parameter
+
+    console.log('Body Parameters');
+    console.log(request.body)
+
+      
+    response.end()
+})
+
+app.get('/:first_name/:last_name/:age', (request, response)=>{
+     //Route Patemeters
+
+    //  console.log('Route Parameters');
+    //  console.log(request.params)
+    //  response.end()
 })
 
 app.listen(3333, () =>{
