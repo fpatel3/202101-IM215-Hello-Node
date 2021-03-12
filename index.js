228dd7c81bf317b2509a7d7bf9960458f9ba0957
fpatel3 <forampatel3010@gmail.com>
2021-03-12 10:15:56 -0500

    updated

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Hi");
+// console.log("Hi");
+
+// person = "Foram"
+
+// charactor = {
+//     name: person,
+//     age: 28,
+// }
+// console.log(person)
+// console.log(charactor)
+ 
+const express = require('express')
+
+const app = express()
+
+app.get('/', (request, response) => {
+
+    console.log("Request received")
+    response.send('Welcome');
+})
+
+app.listen(3333, () =>{
+    console.log("The server is up and running on port 3333")
+})
